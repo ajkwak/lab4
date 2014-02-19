49ccf41f5c1a216a0ca3dcbfd7a791cceacceeea
ajkwak <ajkwak@users.noreply.github.com>
2014-02-19 14:50:57 -0800

    Send email when click on someone's name in MainActivity

--- ClassList/src/edu/mills/cs180a/classlist/MainActivity.java
+++ ClassList/src/edu/mills/cs180a/classlist/MainActivity.java
@@ -2,6 +2,9 @@
 package edu.mills.cs180a.classlist;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -12,9 +15,9 @@ import android.widget.Toast;
 /**
  * An {@code Activity} that displays a list of the names of people in CS 180A.
  * If a name is clicked on, a confirmatory {@link Toast} will be displayed.
- * 
+ *
  * @author ellen.spertus@gmail.com (Ellen Spertus)
- * 
+ *
  * @see Person
  */
 public class MainActivity extends Activity implements ListView.OnItemClickListener  {
@@ -31,7 +34,24 @@ public class MainActivity extends Activity implements ListView.OnItemClickListen
 
     @Override
     public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-        String name = Person.everyone[position].toString();
-        Toast.makeText(this, "Clicked on: " + name, Toast.LENGTH_LONG).show();
+        sendEmailTo(Person.everyone[position]);
+    }
+
+    private void sendEmailTo(Person person) {
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO,
+                Uri.fromParts("mailto", person.getEmail(), null));
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Hello from " + getString(R.string.app_name));
+        String firstName = person.getFirstName();
+        emailIntent.putExtra(Intent.EXTRA_TEXT, new StringBuilder(firstName.length() + 5)
+                .append("Hi, ")
+                .append(firstName)
+                .append('!')
+                .toString());
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send Email"));
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(this, "There are no email clients installed.", Toast.LENGTH_SHORT)
+                    .show();
+        }
     }
 }
