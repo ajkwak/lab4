dc49aaeae2d56386408922ae919ab7378f43b6fc
ajkwak <ajkwak@users.noreply.github.com>
2014-02-21 16:17:32 -0800

    Add package-level javadoc to edu.mills.cs180a.classlist

---
+++ ClassList/src/edu/mills/cs180a/classlist/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains classes used for displaying and sending e-mails to members of CS180A/280A: Mobile
+ * Application Development.
+ *
+ * @author AJ Parmidge
+ */
+package edu.mills.cs180a.classlist;
